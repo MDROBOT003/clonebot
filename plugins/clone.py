e45494df34adc06064fa371731102e2d35945a4f
MDROBOT003 <120124913+MDROBOT003@users.noreply.github.com>
2023-03-20 22:29:15 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -39,9 +39,9 @@ async def clone_medias(client: Bot, message: Message):
     fn_caption = bool(query.file_caption)
     #
     if bool(clone_delay) == bool(1):
-        delay = 30
+        delay = 90
     else:
-        delay = 15
+        delay = 60
     #
     if start_id > end_id:
         start_id = start_id ^ end_id
